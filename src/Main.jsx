cdea3175d0181a55d6dd3ca2048beacaba49d5b4
Abisheksasi <abisheksasi106@gmail.com>
2024-06-14 21:32:01 +0530

    wiret add code

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,8 +1,6 @@
 import Navigation from "./HomePage/Navigation";
-import Referral from "./Component/Referral";
 import HomePage from "./Component/HomePage";
 import Accommodation from "./Component/Accommodation"
-import Trustpilot from "./Component/Trustpilot";
 import Easy from "./Component/EasySteps";
 import House from "./Component/House";
 import Help from "./Component/Help";
@@ -13,9 +11,7 @@ function Main() {
         <div>
            <Navigation/>
            <HomePage/>
-           {/* <Referral/> */}
            <Accommodation/>
-           {/* <Trustpilot/> */}
            <Easy/>
            <House/>
            <Help/>
