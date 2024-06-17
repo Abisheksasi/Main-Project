ae5d052647b309ab5cb99d16037cc5980b2fa2b4
Abisheksasi <abisheksasi106@gmail.com>
2024-06-17 16:06:16 +0530

    add code

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -3,6 +3,7 @@ import { GoVerified } from "react-icons/go";
 import { PiMoneyThin } from "react-icons/pi";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeadset } from '@fortawesome/free-solid-svg-icons';
+
 function Hero() {
     return (
         <div>
@@ -11,28 +12,29 @@ function Hero() {
                 <h6 className='mt-1'>Book student accommodations near top universities and cities across the globe.</h6>
             </div>
             <div className="container heropage">
-                    <div className="justify-content-center g-0 d-flex flex-row">
-                        <div className="mb-2">
-                            <div className="detail rounded-pill d-flex align-items-center mt-1">
-                                <GoVerified className="mt-2 mb-2 ms-3 detail-icon-verify" />
-                                <span className='mt-2 mb-2 ms-2 me-3'>Verified Properties</span>
-                            </div>
+                <div className="justify-content-center d-flex flex-row detail-container">
+                    <div className="mb-2">
+                        <div className="detail rounded-pill d-flex align-items-center mt-1">
+                            <GoVerified className="detail-icon-verify" />
+                            <span className=''>Verified Properties</span>
                         </div>
-                        <div className="ms-2 mb-2">
-                            <div className="detail rounded-pill d-flex align-items-center mt-1">
-                                <FontAwesomeIcon icon={faHeadset} className="mt-2 mb-2 ms-3 detail-icon-head" />
-                                <span className='mt-2 mb-2 ms-2 me-3'>24x7 Assistance</span>
-                            </div>
+                    </div>
+                    <div className="ms-2 mb-2">
+                        <div className="detail rounded-pill d-flex align-items-center mt-1">
+                            <FontAwesomeIcon icon={faHeadset} className=" detail-icon-head" />
+                            <span>24x7 Assistance</span>
                         </div>
-                        <div className="ms-2 mt-1">
-                            <div className="detail rounded-pill d-flex align-items-center">
-                                <PiMoneyThin className="mt-2 mb-2 ms-3 detail-icon-money" />
-                                <span className='mt-2 mb-2 ms-2 me-3'>Price Match Guarantee</span>
-                            </div>
+                    </div>
+                    <div className="ms-2 mt-1">
+                        <div className="detail rounded-pill d-flex align-items-center">
+                            <PiMoneyThin className="detail-icon-money" />
+                            <span>Price Match Guarantee</span>
                         </div>
                     </div>
                 </div>
+            </div>
         </div>
     )
 }
+
 export default Hero;
