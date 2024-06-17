c419eeb24f5fe8f095b175dff4b8f8b3c64bd70e
Abisheksasi <abisheksasi106@gmail.com>
2024-06-17 16:24:03 +0530

    add code

--- src/HomePage/Searchbar.jsx
+++ src/HomePage/Searchbar.jsx
@@ -3,7 +3,7 @@ import './Search.css';
 
 function Search() {
     return (
-        <div className="Search-bar d-flex flex-row justify-content-center mt-5 pb-5 rounded-pill">
+        <div className="Search-bar d-flex flex-row justify-content-center  pb-5 rounded-pill">
             <div className="search-input-container d-flex justify-content-center rounded-pill">
                 <input type="text" className="rounded-pill pt-4 pb-4 ps-4" placeholder="Search by" />
                 <button className="search-containe me-3 rounded-circle d-flex justify-content-center align-items-center" >
